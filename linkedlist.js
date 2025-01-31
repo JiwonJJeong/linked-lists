8c0b0b37b30b51d0adcca9a6ee968540e0bf5e6c
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-01-30 16:20:53 -0800

    Append new nodes to end of linkedlist
    Need to store reference to head
    
    and make function to find tail

--- linkedlist.js
+++ linkedlist.js
@@ -1,6 +1,28 @@
 // LinkedList represents full list
 const LinkedList = function(){
 
+    let headNode = null;
+    const head = function(){
+        return headNode;
+    }
+
+    const tail = function(){
+        let node = headNode.nextNode;
+        while (node.nextNode != null){
+            node = node.nextNode;
+        }
+        return node;
+    }
+    
+    const append = function(value){
+        if (headNode == null){
+            headNode = new Node(value);
+        } else {
+            tail.nextNode = new Node(value);
+        }
+    }
+
+    return {head,tail,append};
 }
 
 // Node contains value property, nextNode
