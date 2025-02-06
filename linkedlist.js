45b521d71eae8f9b14b05299487047889577ff8f
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-02-06 15:26:10 -0800

    Format and find node

--- linkedlist.js
+++ linkedlist.js
@@ -1,158 +1,183 @@
 // LinkedList represents full list
-const LinkedList = function(){
+const LinkedList = function () {
+  let headNode = null;
+  const head = function () {
+    return headNode;
+  };
 
-    let headNode = null;
-    const head = function(){
-        return headNode;
+  const tail = function () {
+    let node = headNode;
+    while (node.nextNode != null) {
+      node = node.nextNode;
     }
+    return node;
+  };
 
-    const tail = function(){
-        let node = headNode;
-        while (node.nextNode != null){
-            node = node.nextNode;
-        }
-        return node;
+  const append = function (value) {
+    if (headNode == null) {
+      headNode = new Node(value);
+    } else {
+      tail().nextNode = new Node(value);
     }
-    
-    const append = function(value){
-        if (headNode == null){
-            headNode = new Node(value);
-        } else {
-            tail().nextNode = new Node(value);
-        }
-    }
+  };
 
-    const prepend = function(value){
-        if (headNode == null){
-            headNode = new Node(value);
-        } else {
-            headNode = new Node(value, headNode);
-        }
+  const prepend = function (value) {
+    if (headNode == null) {
+      headNode = new Node(value);
+    } else {
+      headNode = new Node(value, headNode);
     }
+  };
 
-    const size = function(){
-        let count = 0;
-        if (headNode == null){
-            return count;
-        } else{
-            count++;
-        }
-        let node = headNode;
-        while (node.nextNode != null){
-            node = node.nextNode;
-            count++;
-        }
-        return count;
+  const size = function () {
+    let count = 0;
+    if (headNode == null) {
+      return count;
+    } else {
+      count++;
     }
-
-    // count index from 1
-    const at = function(index){
-        if (index <= 0){
-            return null;
-        }
-        let node = headNode;
-        while (index > 1){
-            node = node.nextNode;
-            index--;
-            if (node == null){
-                return null;
-            }
-        }
-        return node;
+    let node = headNode;
+    while (node.nextNode != null) {
+      node = node.nextNode;
+      count++;
     }
+    return count;
+  };
 
-    const pop = function(){
-        let secondToLastIndex = size() - 1;
-        let secondToLastNode = at(secondToLastIndex);
-        secondToLastNode.nextNode = null;
+  // count index from 1
+  const at = function (index) {
+    if (index <= 0) {
+      return null;
     }
-
-    // returns true if value passed is in list, else returns false
-    const contains = function(value, compareKey = "value"){
-        let node = headNode;
-        while(node != null){
-            if (node.value == value || node.value[compareKey] == value){
-                return true;
-            } else {
-                node = node.nextNode;
-            }
-        }
-        return false;
-    }
-
-    // returns index of node containing value, or null if not found
-    const find = function(value, compareKey = "value"){
-        let node = headNode;
-        let index = 1;
-        while(node != null){
-            if (node.value == value || node.value[compareKey] == value){
-                return index;
-            } else {
-                node = node.nextNode;
-                index++;
-            }
-        }
+    let node = headNode;
+    while (index > 1) {
+      node = node.nextNode;
+      index--;
+      if (node == null) {
         return null;
+      }
     }
+    return node;
+  };
 
-    // returns string of node values in format "(value) -> (value) -> null"
-    const toString = function(){
-        if (headNode == null){
-            return null;
-        }
-        let string = `( ${headNode.value} )`;
-        let node = headNode.nextNode;
-        while (node != null){
-            string += ` -> ( ${node.value} )`;
-            node = node.nextNode;
-        }
-        string += " -> null";
-        return string;
+  const pop = function () {
+    let secondToLastIndex = size() - 1;
+    let secondToLastNode = at(secondToLastIndex);
+    secondToLastNode.nextNode = null;
+  };
+
+  // returns true if value passed is in list, else returns false
+  const contains = function (value, compareKey = "value") {
+    let node = headNode;
+    while (node != null) {
+      if (node.value == value || node.value[compareKey] == value) {
+        return true;
+      } else {
+        node = node.nextNode;
+      }
     }
+    return false;
+  };
 
-    // inserts new Node with value at index (counting from 1)
-    const insertAt = function(value, index){
-        const priorIndex = index-1;
-        if (priorIndex == 0){
-            prepend(value);
-            return;
-        }
-        let priorNode = at(priorIndex);
-        if (priorNode == null){
-            error.log(`${index} is not valid index.`);
-            return;
-        } else {
-            const nextNode = priorNode.nextNode;
-            priorNode.nextNode = new Node(value, nextNode);
-        }
+  // returns index of node containing value, or null if not found
+  const find = function (value, compareKey = "value") {
+    let node = headNode;
+    let index = 1;
+    while (node != null) {
+      if (node.value == value || node.value[compareKey] == value) {
+        return index;
+      } else {
+        node = node.nextNode;
+        index++;
+      }
     }
+    return null;
+  };
 
-    // removes node at index (counting from 1)
-    const removeAt = function(index){
-        const priorIndex = index-1;
-        if (priorIndex == 0){
-            let nodeToRemove = headNode;
-            headNode = headNode.nextNode;
-            nodeToRemove = null;
-            return;
-        }
-        let priorNode = at(priorIndex);
-        let nodeToRemove = priorNode.nextNode;
-        const nextNode = priorNode.nextNode.nextNode;
-        priorNode.nextNode = nextNode;
-        nodeToRemove = null;
+  // same as find, but returns the node, not index
+  const findNode = function (value, compareKey = "value") {
+    let node = headNode;
+    while (node != null) {
+      if (node.value == value || node.value[compareKey] == value) {
+        return node;
+      } else {
+        node = node.nextNode;
+      }
     }
+    return null;
+  };
 
-    return {head,tail,append, prepend, size, at, pop, contains, find, toString, insertAt, removeAt};
-}
+  // returns string of node values in format "(value) -> (value) -> null"
+  const toString = function () {
+    if (headNode == null) {
+      return null;
+    }
+    let string = `( ${headNode.value} )`;
+    let node = headNode.nextNode;
+    while (node != null) {
+      string += ` -> ( ${node.value} )`;
+      node = node.nextNode;
+    }
+    string += " -> null";
+    return string;
+  };
+
+  // inserts new Node with value at index (counting from 1)
+  const insertAt = function (value, index) {
+    const priorIndex = index - 1;
+    if (priorIndex == 0) {
+      prepend(value);
+      return;
+    }
+    let priorNode = at(priorIndex);
+    if (priorNode == null) {
+      error.log(`${index} is not valid index.`);
+      return;
+    } else {
+      const nextNode = priorNode.nextNode;
+      priorNode.nextNode = new Node(value, nextNode);
+    }
+  };
+
+  // removes node at index (counting from 1)
+  const removeAt = function (index) {
+    const priorIndex = index - 1;
+    if (priorIndex == 0) {
+      let nodeToRemove = headNode;
+      headNode = headNode.nextNode;
+      nodeToRemove = null;
+      return;
+    }
+    let priorNode = at(priorIndex);
+    let nodeToRemove = priorNode.nextNode;
+    const nextNode = priorNode.nextNode.nextNode;
+    priorNode.nextNode = nextNode;
+    nodeToRemove = null;
+  };
+
+  return {
+    head,
+    tail,
+    append,
+    prepend,
+    size,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+    insertAt,
+    removeAt,
+  };
+};
 
 // Node contains value property, nextNode
 
-const Node = function(value = null, nextNode = null){
-    return{
-        value,
-        nextNode,
-    }
-}
+const Node = function (value = null, nextNode = null) {
+  return {
+    value,
+    nextNode,
+  };
+};
 
-export {LinkedList, Node};
+export { LinkedList, Node };
